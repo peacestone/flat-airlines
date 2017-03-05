778ce4631b356f7bc446babcbee7b3a59aafbeb8
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-03-04 21:06:23 -0500

    added routes, controllers, and welcome form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :passengers
-  root 'find_flights#find' 
+  root 'welcome#welcome'
+
+  resources 'flights'
+
+  get '/findflights' => 'flights#find', as: :find_flights
+  post '/find-flights', to: 'flights#found', as: "found_flights"
+
+  post '/reservations' => 'reservations#create', as: :create_reservation
+
+    resources 'passengers', only: :show do
+      resources 'reservations'
+    end
+
 end
